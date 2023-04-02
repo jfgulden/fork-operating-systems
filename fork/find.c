90f467ea0d48591b78a2ffb54908d59f3e8359e4
root <root@LAPTOP-LVJG9BR8.localdomain>
2023-04-02 18:59:16 -0300

    Ejercicio listo para entregar

--- fork/find.c
+++ fork/find.c
@@ -17,86 +17,105 @@
 
 char *strcasestr(const char *haystack, const char *needle);
 
-bool es_directorio_actual_especial(char* nombre){
-    return (strcmp(nombre, DIR_ACTUAL) == 0 || strcmp(nombre, DIR_SUPERIOR) == 0);
+bool
+es_directorio_actual_especial(char *nombre)
+{
+	return (strcmp(nombre, DIR_ACTUAL) == 0 ||
+	        strcmp(nombre, DIR_SUPERIOR) == 0);
 }
 
-int buscar_archivo(DIR *dir, char path_actual[], char archivo_buscado[], char* (*comparar_substring)(const char*, const char*)){
-    struct dirent *acceso_dir = readdir(dir);
-    int fd_acceso_dir = dirfd(dir);
-    if (fd_acceso_dir == ERROR){
-        printf("Error al intentar obtener el file descriptor del directorio\n");
-        return ERROR;
-    }
-    
-    
-    while(acceso_dir != NULL){
+int
+buscar_archivo(DIR *dir,
+               char path_actual[],
+               char archivo_buscado[],
+               char *(*comparar_substring)(const char *, const char *) )
+{
+	struct dirent *acceso_dir = readdir(dir);
+	int fd_acceso_dir = dirfd(dir);
+	if (fd_acceso_dir == ERROR) {
+		printf("Error al intentar obtener el file descriptor del "
+		       "directorio\n");
+		return ERROR;
+	}
 
-        if (es_directorio_actual_especial(acceso_dir->d_name)){
-            acceso_dir = readdir(dir);
-            continue;
-        }
-        if (comparar_substring(acceso_dir->d_name, archivo_buscado) != NULL){
-            printf("%s%s\n", path_actual, acceso_dir->d_name);
-        }
-        if (acceso_dir->d_type == DT_DIR){
-            char subdir_path[PATH_MAX] = "\0";
-            strcpy(subdir_path, path_actual);
-            strcat(subdir_path, acceso_dir->d_name);
-            strcat(subdir_path, "/");
 
-            int fd_acceso_subdir = openat(fd_acceso_dir, acceso_dir->d_name, O_DIRECTORY);
-            if (fd_acceso_dir == ERROR){
-                printf("Error al intentar abrir el subdirectorio\n");
-                return ERROR;
-            }
-            DIR *subdir = fdopendir(fd_acceso_subdir);
-            if (!subdir){
-                printf("Error al intentar obtener acceso al subdirectorio\n");
-                return ERROR;
-            }
-            int resultado_busqueda = buscar_archivo(subdir, subdir_path, archivo_buscado, comparar_substring);
-            if (resultado_busqueda == ERROR)
-                return ERROR;
-        
-            closedir(subdir);
-        }
-        acceso_dir = readdir(dir);
-    }
-    return 0;
-}
-int main(int argc, char *argv[]){
-    if (argc < ARGS_MINIMOS) {
-        printf("Error al leer la línea\n");
-        exit (-1);
-    }
-    char path[PATH_MAX] = "\0";
-    char *archivo_buscado = NULL; 
-    char* (*comparar_substring)(const char*, const char*) = NULL;
-    DIR *dir = opendir(DIR_ACTUAL); 
-    int resultado_busqueda = 0;
+	while (acceso_dir != NULL) {
+		if (es_directorio_actual_especial(acceso_dir->d_name)) {
+			acceso_dir = readdir(dir);
+			continue;
+		}
+		if (comparar_substring(acceso_dir->d_name, archivo_buscado) !=
+		    NULL) {
+			printf("%s%s\n", path_actual, acceso_dir->d_name);
+		}
+		if (acceso_dir->d_type == DT_DIR) {
+			char subdir_path[PATH_MAX] = "\0";
+			strcpy(subdir_path, path_actual);
+			strcat(subdir_path, acceso_dir->d_name);
+			strcat(subdir_path, "/");
 
-    if (!dir){
-        printf("Error al abrir el directorio\n");
-        exit(-1);
-    }
-    
-    if (argc == 3 && strcmp(argv[1], CASE_FLAG) == 0){
-        archivo_buscado = argv[2];
-        comparar_substring = strcasestr;
-    }
-    if (argc == 2){
-        archivo_buscado = argv[1];
-        comparar_substring = strstr;
-    }
-    resultado_busqueda = buscar_archivo(dir, path, archivo_buscado, comparar_substring);
+			int fd_acceso_subdir = openat(fd_acceso_dir,
+			                              acceso_dir->d_name,
+			                              O_DIRECTORY);
+			if (fd_acceso_dir == ERROR) {
+				printf("Error al intentar abrir el "
+				       "subdirectorio\n");
+				return ERROR;
+			}
+			DIR *subdir = fdopendir(fd_acceso_subdir);
+			if (!subdir) {
+				printf("Error al intentar obtener acceso al "
+				       "subdirectorio\n");
+				return ERROR;
+			}
+			int resultado_busqueda =
+			        buscar_archivo(subdir,
+			                       subdir_path,
+			                       archivo_buscado,
+			                       comparar_substring);
+			if (resultado_busqueda == ERROR)
+				return ERROR;
 
-    closedir(dir);
-    if (resultado_busqueda == ERROR){
-        printf("Error al intentar buscar el archivo\n");
-        return ERROR;
-    }
-    
+			closedir(subdir);
+		}
+		acceso_dir = readdir(dir);
+	}
 	return 0;
 }
+int
+main(int argc, char *argv[])
+{
+	if (argc < ARGS_MINIMOS) {
+		printf("Error al leer la línea\n");
+		exit(-1);
+	}
+	char path[PATH_MAX] = "\0";
+	char *archivo_buscado = NULL;
+	char *(*comparar_substring)(const char *, const char *) = NULL;
+	DIR *dir = opendir(DIR_ACTUAL);
+	int resultado_busqueda = 0;
 
+	if (!dir) {
+		printf("Error al abrir el directorio\n");
+		exit(-1);
+	}
+
+	if (argc == 3 && strcmp(argv[1], CASE_FLAG) == 0) {
+		archivo_buscado = argv[2];
+		comparar_substring = strcasestr;
+	}
+	if (argc == 2) {
+		archivo_buscado = argv[1];
+		comparar_substring = strstr;
+	}
+	resultado_busqueda =
+	        buscar_archivo(dir, path, archivo_buscado, comparar_substring);
+
+	closedir(dir);
+	if (resultado_busqueda == ERROR) {
+		printf("Error al intentar buscar el archivo\n");
+		return ERROR;
+	}
+
+	return 0;
+}
--- fork/xargs.c
+++ fork/xargs.c
@@ -15,69 +15,74 @@
 int const ARGS_MINIMOS = 2;
 int const ERROR = -1;
 
-void liberar_args(char *args[TAM_ARRAY], int tope){
-    for (int i = 1; i < (tope-1); i++){     //rgs[tope] = NULL. Por eso libero hasta tope-1
-        free(args[i]);
-        args[i] = NULL;
-    }
+void
+liberar_args(char *args[TAM_ARRAY], int tope)
+{
+	for (int i = 1; i < (tope - 1);
+	     i++) {  // rgs[tope] = NULL. Por eso libero hasta tope-1
+		free(args[i]);
+		args[i] = NULL;
+	}
 }
-int ejecutar_comando(char *args[TAM_ARRAY], int* tope){
-    args[NARGS+1] = NULL;
-    (*tope)++;
+int
+ejecutar_comando(char *args[TAM_ARRAY], int *tope)
+{
+	args[NARGS + 1] = NULL;
+	(*tope)++;
 
-    int f = fork();
-    if (f < 0){
-        printf("Error al crear el proceso hijo");
-        exit(ERROR);
-    }
-    if (f == 0){
-        if (execvp(args[0], args) == ERROR){
-            printf("Error al hacer execvp\n");
-            return ERROR;
-        }
-    }
-    if (f > 0){
-
-        wait(NULL);
-    }
-    return 0;
+	int f = fork();
+	if (f < 0) {
+		printf("Error al crear el proceso hijo");
+		exit(ERROR);
+	}
+	if (f == 0) {
+		if (execvp(args[0], args) == ERROR) {
+			printf("Error al hacer execvp\n");
+			return ERROR;
+		}
+	}
+	if (f > 0) {
+		wait(NULL);
+	}
+	return 0;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < ARGS_MINIMOS) {
-        printf("Error al leer la línea\n");
-        exit (ERROR);
-    }
-    
-    size_t tam = 0;
-    char *linea = NULL;
-    char *args[TAM_ARRAY] = {argv[1]};  //TAM_ARRAY = 6 porque en la última posición va NULL y en la primera va argv[1] -> ls
-    int tope = 1;
+int
+main(int argc, char *argv[])
+{
+	if (argc < ARGS_MINIMOS) {
+		printf("Error al leer la línea\n");
+		exit(ERROR);
+	}
 
-    while(getline(&linea, &tam, stdin) != ERROR){
+	size_t tam = 0;
+	char *linea = NULL;
+	char *args[TAM_ARRAY] = {
+		argv[1]
+	};  // TAM_ARRAY = 6 porque en la última posición va NULL y en la primera va argv[1] -> ls
+	int tope = 1;
 
-        linea[strlen(linea)-1] = '\0';  //Elimino el /n 
-        args[tope] = linea;             
-        tope++;
-        linea = NULL;
+	while (getline(&linea, &tam, stdin) != ERROR) {
+		linea[strlen(linea) - 1] = '\0';  // Elimino el /n
+		args[tope] = linea;
+		tope++;
+		linea = NULL;
 
-      if (tope == (NARGS+1)){
-        //Se ejecuta el comando una vez que se leyeron 4 argumentos
-        if (ejecutar_comando(args, &tope) == ERROR){ 
-            exit(ERROR); 
-        }
-        liberar_args(args, tope);
-        tope = 1;
-      }
-    }
-    if (tope > 0){
-        //Si quedan argumentos sin ser ejecutados, se ejecutan
-        if (ejecutar_comando(args, &tope) == ERROR)
-            exit(ERROR);
-        liberar_args(args, tope);
-    }
-    free(linea);
+		if (tope == (NARGS + 1)) {
+			// Se ejecuta el comando una vez que se leyeron 4 argumentos
+			if (ejecutar_comando(args, &tope) == ERROR) {
+				exit(ERROR);
+			}
+			liberar_args(args, tope);
+			tope = 1;
+		}
+	}
+	if (tope > 0) {
+		// Si quedan argumentos sin ser ejecutados, se ejecutan
+		if (ejecutar_comando(args, &tope) == ERROR)
+			exit(ERROR);
+		liberar_args(args, tope);
+	}
+	free(linea);
 	exit(0);
 }
-
-
